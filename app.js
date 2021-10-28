18d5a13308624dff7f03549717df4f452059b0fc
fabu8cc <93300864+fabu8cc@users.noreply.github.com>
2021-10-28 10:44:58 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+document.write("<style>");
+document.write(".links-top{overflow:hidden;margin:auto;background:#2c2e2f;box-shadow:0 3px 6px 2px rgba(0,0,0,.03)}");
+document.write(".links-top ul li{float:left;padding:10px 0;width:10%;text-align:center}");
+document.write(".links-top ul li img{margin:0 auto;padding:4px;width:56px;height:56px;border-radius:15px;}");
+document.write("\/*.links-top ul li img{background-color: #666666;-webkit-transition: background-color .4s;transition: background-color .4s;}*\/");
+document.write(".links-top ul li img:hover{background-color:#FF9800;}");
+document.write(".links-top ul a{font-size: 14px;color:#bdbdbd;}");
+document.write(".links-top ul a:hover{color:#FF9800;}");
+document.write("@media (max-width:544px){.links{padding:0 5px}");
+document.write("");
+document.write(".links-top ul li{width:25%}");
+document.write(".links-box .panel-heading .btn{padding:0 10px}");
+document.write(".links-element ul{overflow:hidden;padding:10px 0 10px 10px}");
+document.write(".links-top ul a{font-size: 13px;}");
+document.write("<\/style>");
+document.write("<div class=\"links-top\">");
+document.write("<ul>");
+document.write("<li><a href=\"https:\/\/app.highgg.com?agentId=13694519\" target=\"_blank\"><img src=\"http:\/\/tva1.sinaimg.cn\/large\/007YsmfJgy1gvqpjvs7lpj60b40b40sl02.jpg\"><br><span>快猫<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/cmdhwb.com\/?dc=rga00439\" target=\"_blank\"><img src=\"https:\/\/s3.bmp.ovh\/imgs\/2021\/10\/028675f4671071c3.png\"><br><span>麻豆传媒<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/blqd01.com\/?dc=bsqa6\" target=\"_blank\"><img src=\"http:\/\/ch.madouwersdf.com\/static\/img\/logo.9e60c06f.png\"><br><span>逼哩逼哩<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/zgmyws.com\/?code=R0QJ\" target=\"_blank\"><img src=\"http:\/\/ch.madouwersdf.com\/static\/img\/logo.9e60c06f.png\"><br><span>日女<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/7u.soupenggame.com\/chan-1478\/aff-mkeP\" target=\"_blank\"><img src=\"https:\/\/new_img.ycomesc.com\/new\/ads\/20210401\/2021040115061366000.jpeg\"><br><span>妻友<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/dsp.soupenggame.com\/chan-1884\/aff-fHWQY\" target=\"_blank\"><img src=\"https:\/\/new_img.ycomesc.com\/new\/ads/20200428\/2020042817261828010.png\"><br><span>成人抖音视频<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/bo.soupenggame.com\/chan\/bu1200/5JVG\" target=\"_blank\"><img src=\"http:\/\/imgpublic.ycomesc.com\/new\/ads\/20210731\/2021073118561411859.gif\"><br><span>菠萝直播免费版<\/span><\/a><\/li>");
+
+document.write("<li><a href=\"https:\/\/gv.soupenggame.com\/chan\/xb1295\/RpNw\" target=\"_blank\"><img src=\"http:\/\/new_img.ycomesc.com\/new\/ads\/20201208\/2020120822055221300.png\"><br><span>小蓝俱乐部<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/50.soupenggame.com\/chan\/h51466\/bDYRv\" target=\"_blank\"><img src=\"http:\/\/imgpublic.ycomesc.com\/new/ads\/20210818\/2021081821285786424.gif\"><br><span>成人B站猎奇版<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/didi.soupenggame.com\/chan-1705\/aff-8HAc\" target=\"_blank\"><img src=\"http:\/\/new_img.ycomesc.com\/new\/head\/20200605\/2020060519062420252.png\"><br><span>91滴滴<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/cg.soupenggame.com\/?code=ZMNF&c=2717\" target=\"_blank\"><img src=\"http:\/\/imgpublic.ycomesc.com\/new\/ads\/20210818\/2021081821282618549.gif\"><br><span>茶馆微信约炮<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/mv.soupenggame.com\/chan-1807\/aff-bJsQs\" target=\"_blank\"><img src=\"http:\/\/new_img.ycomesc.com\/new\/ads\/20200428\/2020042816575015948.png\"><br><span>91Porn金币破解版<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/ttt.wtfxx.org\/chan\/a11350\/aDU8X\" target=\"_blank\"><img src=\"http:\/\/new_img.ycomesc.com/new\/ads\/20210323\/2021032317133736758.png\"><br><span>汤头条<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/ttt.soupenggame.com\/chan\/a11350\/aDU8X\" target=\"_blank\"><img src=\"http:\/\/imgpublic.ycomesc.com\/new\/ads\/20210818\/2021081821270395109.gif\"><br><span>今日头条成人版<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/zpc.soupenggame.com\/chan-1527\/aff-gCSU\" target=\"_blank\"><img src=\"http:\/\/imgpublic.ycomesc.com\/new\/ads\/20210624\/2021062419444530862.png\"><br><span>91制片厂<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/sir.soupenggame.com\/c-2717\/a-a38Ng\" target=\"_blank\"><img src=\"http:\/\/imgpublic.ycomesc.com\/new\/ads\/20210827\/2021082710533743313.png\"><br><span>撸先生<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/51mh046.com\/?_c=octt9mh\" target=\"_blank\"><img src=\"http:\/\/alppagent.com\/upload\/default\/2020\/08\/06\/bcG80ZXx5SNFSvOjPxwgMcB2AKtcJbm6PsJYR1tl.png\"><br><span>漫画<\/span><\/a><\/li>");
+
+
+document.write("<\/ul>");
+document.write("<\/div>");
