fbbbd960ce3d2356cfa0e13869bf6bc4bc13d861
fabu8cc <93300864+fabu8cc@users.noreply.github.com>
2021-11-05 20:41:03 +0800

    Add files via upload

---
+++ app_a.js
@@ -0,0 +1,25 @@
+document.write("<style>");
+document.write(".friendItem{display: inline-block;font-size:13px ;width: 10%;padding: 4px;text-align: center;border: 1px solid #ffffff;}");
+document.write("@media screen and (max-device-width: 760px){.friendItem {width: 25%!important;}}");
+document.write(".friendItemA{color: #ffffff;}");
+
+document.write("<\/style>");
+
+document.write("<ul style=\"background: linear-gradient(90deg, #0f7884 0%, #0f7884 100%);\">");
+
+// document.write("<li class=\"friendItem\"><a href=\"https:\/\/91qz05.me\/?_c=ofzl1qz\" target=\"_blank\" rel='nofollow' onclick=\"window.open('/chain/8534968217582c0cf0175c5201a5b75ce');return false;\" class=\"friendItemA\">91茄子专享</a></li>");
+
+document.write("<li class=\"friendItem\"><a href=\"https:\/\/91qz06.me\/?_c=ofzl1qz\" target=\"_blank\" rel='nofollow' style class=\"friendItemA\">91茄子专享</a></li>");
+document.write("<li class=\"friendItem\"><a href=\"https:\/\/bitch020.com\/?_c=ofzl1my\" target=\"_blank\" rel='nofollow' style class=\"friendItemA\">名媛专享</a></li>");
+document.write("<li class=\"friendItem\"><a href=\"https:\/\/k9sm28.com\/?_c=ofzl1k9\" target=\"_blank\" rel='nofollow' style class=\"friendItemA\">k9专享</a></li>");
+document.write("<li class=\"friendItem\"><a href=\"https:\/\/kuaise5.me\/?channel=ofzl1ks\" target=\"_blank\" rel='nofollow' style class=\"friendItemA\">快色专享</a></li>");
+document.write("<li class=\"friendItem\"><a href=\"https:\/\/tx5274.com\/?_c=ofzl1tx\" target=\"_blank\" rel='nofollow' style class=\"friendItemA\">糖心专享</a></li>");
+document.write("<li class=\"friendItem\"><a href=\"https:\/\/av.ccav69h.com\/?channel=ofzl1av\" target=\"_blank\" rel='nofollow' style class=\"friendItemA\">CCAV专享</a></li>");
+document.write("<li class=\"friendItem\"><a href=\"https:\/\/hj04.me\/?_c=ofzl1hj\" target=\"_blank\" rel='nofollow' style class=\"friendItemA\">花椒专享</a></li>");
+document.write("<li class=\"friendItem\"><a href=\"https:\/\/51mh061.com\/?_c=ofzl1mh\" target=\"_blank\" rel='nofollow' style class=\"friendItemA\">51漫画专享</a></li>");
+document.write("<li class=\"friendItem\"><a href=\"https:\/\/xiuse051.com\/?_c=ofzl1xs\" target=\"_blank\" rel='nofollow' style class=\"friendItemA\">秀色专享</a></li>");
+document.write("<li class=\"friendItem\"><a href=\"https:\/\/xb19.me\/?_c=ofzl1xb\" target=\"_blank\" rel='nofollow' style class=\"friendItemA\">杏吧专享</a></li>");
+
+
+
+document.write("<div class=\"clear\"></div><\/ul>");
