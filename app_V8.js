08e7cb3fb445f9db90a77877067a1d8523a90505
fabu8cc <93300864+fabu8cc@users.noreply.github.com>
2021-11-18 10:08:32 +0800

    Add files via upload

---
+++ app_V8.js
@@ -0,0 +1,88 @@
+document.write("<style>");
+document.write(".links-top{overflow:hidden;margin:auto;background:#2c2e2f;box-shadow:0 3px 6px 2px rgba(0,0,0,.03)}");
+document.write(".links-top ul li{float:left;padding:10px 0;width:10%;text-align:center}");
+document.write(".links-top ul li img{margin:0 auto;padding:4px;width:56px;height:56px;border-radius:15px;}");
+document.write("\/*.links-top ul li img{background-color: #666666;-webkit-transition: background-color .4s;transition: background-color .4s;}*\/");
+document.write(".links-top ul li img:hover{background-color:#FF9800;}");
+document.write(".links-top ul a{font-size: 14px;color:#bdbdbd;}");
+document.write(".links-top ul a:hover{color:#FF9800;}");
+document.write("@media (max-width:544px){.links{padding:0 5px}");
+document.write("");
+document.write(".links-top ul li{width:25%}");
+document.write(".links-box .panel-heading .btn{padding:0 10px}");
+document.write(".links-element ul{overflow:hidden;padding:10px 0 10px 10px}");
+document.write(".links-top ul a{font-size: 13px;}");
+document.write("<\/style>");
+document.write("<div class=\"links-top\">");
+document.write("<ul>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_1.html\" target=\"_blank\"><img src=\"http:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E5%BF%AB%E7%8C%AB.jpg\"><br><span>快猫<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_2.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E9%BA%BB%E8%B1%86.png\"><br><span>麻豆传媒<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_3.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E9%80%BC%E5%93%A9.png\"><br><span>逼哩逼哩<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_4.html\" target=\"_blank\"><img src=\"http:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E8%8C%B6%E9%A6%86%E5%BE%AE%E4%BF%A1%E7%BA%A6%E7%82%AE.gif\"><br><span>日女<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_5.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E5%A6%BB%E5%8F%8B.jpg\"><br><span>杏吧<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_6.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E6%88%90%E4%BA%BA%E6%8A%96%E9%9F%B3%E8%A7%86%E9%A2%91.png\"><br><span>成人抖音视频<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_7.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E8%8F%A0%E8%90%9D%E7%9B%B4%E6%92%AD%E5%85%8D%E8%B4%B9%E7%89%88.gif\"><br><span>原味视频<\/span><\/a><\/li>");
+
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_8.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E5%B0%8F%E8%93%9D%E4%BF%B1%E4%B9%90%E9%83%A8.png\"><br><span>K9俱乐部<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_9.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E7%B3%96%E5%BF%83.png\"><br><span>糖心<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_10.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/91%E5%8A%A8%E6%BC%AB.gif\"><br><span>91动漫<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_11.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E8%8C%B6%E9%A6%86%E5%BE%AE%E4%BF%A1%E7%BA%A6%E7%82%AE.gif\"><br><span>茶馆微信约炮<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_12.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/91Porn%E9%87%91%E5%B8%81%E7%A0%B4%E8%A7%A3%E7%89%88.png\"><br><span>91Porn金币破解版<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_13.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E6%B1%A4%E5%A4%B4%E6%9D%A1.png\"><br><span>汤头条<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_14.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E6%B3%A1%E8%8A%99%E7%9F%AD%E8%A7%86%E9%A2%91.jpg\"><br><span>泡芙短视频<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_15.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/91%E7%BA%A6%E7%82%AE.png\"><br><span>91约炮<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_16.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E8%89%B2%E4%B8%AD%E8%89%B2.png\"><br><span>色中色<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_17.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/51%E6%BC%AB%E7%94%BB.png\"><br><span>漫画<\/span><\/a><\/li>");
+
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_18.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/9%E5%8F%8B.jpg\"><br><span>9友<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_19.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E9%9B%8F%E9%B8%9F.jpg\"><br><span>雏鸟<\/span><\/a><\/li>");
+document.write("<li><a href=\"https:\/\/maomi.neocities.org\/cp\/cp_20.html\" target=\"_blank\"><img src=\"http:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E5%8E%9F%E5%91%B3%E8%A7%86%E9%A2%91.png\"><br><span>原味女孩<\/span><\/a><\/li>");
+
+
+document.write("<\/ul>");
+document.write("<\/div>");
+
+
+// document.write("<style>");
+// document.write(".links-top{overflow:hidden;margin:auto;background:#2c2e2f;box-shadow:0 3px 6px 2px rgba(0,0,0,.03)}");
+// document.write(".links-top ul li{float:left;padding:10px 0;width:10%;text-align:center}");
+// document.write(".links-top ul li img{margin:0 auto;padding:4px;width:56px;height:56px;border-radius:15px;}");
+// document.write("\/*.links-top ul li img{background-color: #666666;-webkit-transition: background-color .4s;transition: background-color .4s;}*\/");
+// document.write(".links-top ul li img:hover{background-color:#FF9800;}");
+// document.write(".links-top ul a{font-size: 14px;color:#bdbdbd;}");
+// document.write(".links-top ul a:hover{color:#FF9800;}");
+// document.write("@media (max-width:544px){.links{padding:0 5px}");
+// document.write("");
+// document.write(".links-top ul li{width:25%}");
+// document.write(".links-box .panel-heading .btn{padding:0 10px}");
+// document.write(".links-element ul{overflow:hidden;padding:10px 0 10px 10px}");
+// document.write(".links-top ul a{font-size: 13px;}");
+// document.write("<\/style>");
+// document.write("<div class=\"links-top\">");
+// document.write("<ul>");
+// document.write("<li><a href=\"https:\/\/app.highgg.com?agentId=13694519\" target=\"_blank\"><img src=\"http:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E5%BF%AB%E7%8C%AB.jpg\"><br><span>快猫<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/cmdhwb.com\/?dc=rga00439\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E9%BA%BB%E8%B1%86.png\"><br><span>麻豆传媒<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/rterdjklm.com\/?dc=bsqa6\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E9%80%BC%E5%93%A9.png\"><br><span>逼哩逼哩<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/rterdjklm.com\/?dc=bsqa6\" target=\"_blank\"><img src=\"http:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E8%8C%B6%E9%A6%86%E5%BE%AE%E4%BF%A1%E7%BA%A6%E7%82%AE.gif\"><br><span>日女<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/xb21.me\/?_c=ofzl1xb\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E5%A6%BB%E5%8F%8B.jpg\"><br><span>杏吧<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/pfldyshr.com?dc=TANT\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E6%88%90%E4%BA%BA%E6%8A%96%E9%9F%B3%E8%A7%86%E9%A2%91.png\"><br><span>成人抖音视频<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/nk.bike-sleep.com\/download\/56_0.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E8%8F%A0%E8%90%9D%E7%9B%B4%E6%92%AD%E5%85%8D%E8%B4%B9%E7%89%88.gif\"><br><span>原味视频<\/span><\/a><\/li>");
+
+// document.write("<li><a href=\"https:\/\/k9sm28.com\/?_c=ofzl1k9\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E5%B0%8F%E8%93%9D%E4%BF%B1%E4%B9%90%E9%83%A8.png\"><br><span>K9俱乐部<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/tx5274.com\/?_c=ofzl1tx\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E7%B3%96%E5%BF%83.png\"><br><span>糖心<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/51mh061.com/?_c=ofzl1mh\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/91%E5%8A%A8%E6%BC%AB.gif\"><br><span>91动漫<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/nk.bike-sleep.com\/download\/56_0.html\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E8%8C%B6%E9%A6%86%E5%BE%AE%E4%BF%A1%E7%BA%A6%E7%82%AE.gif\"><br><span>茶馆微信约炮<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/rterdjklm.com\/?dc=bsqa6\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/91Porn%E9%87%91%E5%B8%81%E7%A0%B4%E8%A7%A3%E7%89%88.png\"><br><span>91Porn金币破解版<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/rterdjklm.com\/?dc=bsqa6\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E6%B1%A4%E5%A4%B4%E6%9D%A1.png\"><br><span>汤头条<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/pfldyshr.com?dc=TANT\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E6%B3%A1%E8%8A%99%E7%9F%AD%E8%A7%86%E9%A2%91.jpg\"><br><span>泡芙短视频<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/91ycp.live?dc=TANT\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/91%E7%BA%A6%E7%82%AE.png\"><br><span>91约炮<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/seiseas.com\/?dc=TANT\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E8%89%B2%E4%B8%AD%E8%89%B2.png\"><br><span>色中色<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/51mh061.com\/?_c=ofzl1mh\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/51%E6%BC%AB%E7%94%BB.png\"><br><span>漫画<\/span><\/a><\/li>");
+
+// document.write("<li><a href=\"https:\/\/9y03.me\/?code=&channel=bm740621\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/9%E5%8F%8B.jpg\"><br><span>9友<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/cn03.me\/?code=&channel=bm740621\" target=\"_blank\"><img src=\"https:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E9%9B%8F%E9%B8%9F.jpg\"><br><span>雏鸟<\/span><\/a><\/li>");
+// document.write("<li><a href=\"https:\/\/nk.bike-sleep.com\/download\/56_0.html\" target=\"_blank\"><img src=\"http:\/\/cdn.jsdelivr.net\/gh\/fabu8cc\/static\/%E5%8E%9F%E5%91%B3%E8%A7%86%E9%A2%91.png\"><br><span>原味女孩<\/span><\/a><\/li>");
+
+
+// document.write("<\/ul>");
+// document.write("<\/div>");
